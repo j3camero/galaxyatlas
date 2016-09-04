f491714e226c6e127b7d74fe541d9c2bc9ad367a
Jeff Cameron <jecameron@expedia.com>
2016-09-03 17:18:39 -0700

    An experiment to see what different star rendering methods look decent.

--- client.js
+++ client.js
@@ -1,16 +1,19 @@
 
-function DrawStar(context, x, y, brightness) {
-    context.fillStyle = "rgba(255,255,128,0.5)";
+function DrawStar(context, x, y, radius, alpha) {
+    context.fillStyle = "rgba(255,255,128," + alpha + ")";
     context.beginPath();
-    context.arc(x, y, brightness, 0, 2 * Math.PI);
+    context.arc(x, y, radius, 0, 2 * Math.PI);
     context.fill();
 }
 
-setTimeout(function() {
+setInterval(function() {
     var canvas = document.getElementById("stars");
     var context = canvas.getContext("2d");
-    console.log(canvas.width + " " + canvas.height);
     context.fillStyle = "black";
     context.fillRect(0, 0, canvas.width, canvas.height);
-    DrawStar(context, 10, 10, 7);
-}, 3000);
+    for (var i = 1; i < 20; ++i) {
+	for (var j = 1; j < 20; ++j) {
+	    DrawStar(context, 10 * i, 10 * j, 0.1 * i, 0.1 * j);
+	}
+    }
+}, 1000);
