3d518ff8d26d58e38ded57222b4aec7830e2759e
Jeff Cameron <jecameron@expedia.com>
2016-09-10 18:34:19 -0700

    Starting star brightness calibration.

--- client.js
+++ client.js
@@ -13,7 +13,31 @@ setInterval(function() {
     context.fillRect(0, 0, canvas.width, canvas.height);
     for (var i = 1; i < 20; ++i) {
 	for (var j = 1; j < 20; ++j) {
-	    DrawStar(context, 10 * i, 10 * j, 0.1 * i, 0.1 * j);
+	    DrawStar(context,
+		     10 * i + Math.random(),
+		     10 * j + Math.random(),
+		     0.05 * i,
+		     0.05 * j);
 	}
     }
 }, 1000);
+
+function MakeBlankCanvas(width, height, color) {
+    var canvas = document.createElement("canvas");
+    canvas.width = width;
+    canvas.height = height;
+    var context = canvas.getContext("2d");
+    context.fillStyle = color;
+    context.fillRect(0, 0, width, height);
+    return canvas;
+}
+
+function TotalBrightness(canvas) {
+    var context = canvas.getContext("2d");
+    var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    for (var i = 0; i < imgageData.data.length; i += 4) {
+	var brightness = Math.max(imageData.data[i],
+				  imageData.data[i + 1],
+				  imageData.data[i + 2]);
+    }
+}
