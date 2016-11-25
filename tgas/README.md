5799b972a49019e9dcaa30034b139b251b26982e
Jeff Cameron <jecameron@expedia.com>
2016-11-24 22:35:48 -0800

    Script for merging TGAS and Hipparcos catalogs.

--- tgas/README.md
+++ tgas/README.md
@@ -3,3 +3,8 @@ or so stars whose 3D positions have been released as part of Gaia Data
 Release #1.
 
 These are tools for dealing with the TGAS dataset.
+
+sh download_tgas.sh
+python convert_stars.py
+python convert_hipparcos.py
+python merge_tgas_and_hipparcos.py
---
+++ tgas/merge_tgas_and_hipparcos.py
@@ -0,0 +1,49 @@
+import csv
+import sys
+
+output_csv_filename = 'thc.csv'
+
+def WriteOneStar(csv_row_as_list):
+    with open(output_csv_filename, 'a') as output_file:
+        writer = csv.writer(output_file)
+        writer.writerow(csv_row_as_list)
+
+# Load the Hipparcos stars, keyed by their HIP numbers.
+hipparcos = {}
+with open('hipparcos.csv') as input_file:
+    input_file.readline()  # Skip csv headers.
+    reader = csv.reader(input_file)
+    for row in reader:
+        designation = row[0]
+        hipparcos[designation] = row
+
+# Clear the output file.
+with open(output_csv_filename, 'w') as f:
+    pass
+
+# Write the TGAS stars, removing any stars that overlap from Hipparcos.
+WriteOneStar(['THC', 'designation', 'x', 'y', 'z',
+              'luminosity', 'red', 'green', 'blue'])
+star_count = 1
+with open('tgas.csv') as input_file:
+    input_file.readline()  # Skip csv headers.
+    reader = csv.reader(input_file)
+    for row in reader:
+        row.insert(0, star_count)
+        star_count += 1
+        WriteOneStar(row)
+        if reader.line_num % 100000 == 0:
+            sys.stdout.write('.')
+            sys.stdout.flush()
+        designation = row[0]
+        hipparcos.pop(designation, None)
+
+# Write the remaining Hipparcos stars, that weren't eliminated for
+# overlapping with TGAS.
+for hip in hipparcos:
+    row = hipparcos[hip]
+    row.insert(0, star_count)
+    star_count += 1
+    WriteOneStar(row)
+
+print ''
