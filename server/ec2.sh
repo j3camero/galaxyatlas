e4efaba3106f642f5f8eed78fa8eafa1d1d1a40c
Matthew Thiffault <matt.thiffault@gmail.com>
2016-12-11 17:20:11 -0800

    Fix ec2 deploy script.

--- server/ec2.sh
+++ server/ec2.sh
@@ -6,7 +6,7 @@ apt-get -y install build-essential libboost-all-dev libeigen3-dev \
   libjsoncpp-dev make
 echo "Cloning latest code from GitHub."
 git clone --recursive https://github.com/j3camero/galaxyatlas
-cd galaxyatlas/nativeatlas/
+cd galaxyatlas/server/
 sed -i 's/8080/80/' main.cc
 echo "Compiling the webserver."
 make
