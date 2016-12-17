9bc4098a0f011f716e96377eac62856937672009
Matthew Thiffault <matt.thiffault@gmail.com>
2016-12-17 11:40:17 -0800

    Have 3d motion controls working with test cube.

--- server/static/client.js
+++ server/static/client.js
@@ -1,23 +1,30 @@
 // Constants
+var minNearFieldLum = 0.001;
 var distUpdateThreshold = 2;
 var speed = 0.3;
 var turnRate = 0.03;
 
 var visibleOctants = [];
 var octantDict = {};
-
 var starsUpdated = false;
-var cameraPosition = new Vector(0, 0, 0);
-var lastUpdatePosition = new Vector(0, 0, 0);
-var lastRenderPosition = new Vector(0, 0, 0);
-var cameraDirection = new Vector(0, 1, 0);
-var lastRenderDirection = new Vector(0, 1, 0);
-var upDirection = new Vector(0, 0, 1);
+
+// Camera Position and Rotation
+var initialCameraPosition = new THREE.Vector3(0, 0, 3);
+var initialCameraRotation = new THREE.Quaternion(0, 0, 0, 1);
+
+var lastUpdatePosition = new THREE.Vector3(0, 0, 0);
+
+// Variables for keyboard/touchscreen controls
 var keys = new Array();
 var draggingCanvas = false;
 var dragVector = {x: null, y: null};
 var pressForward = false;
 var activeDrag = false;
+
+// Three.js global objects
+var scene = null;
+var camera = null;
+var renderer = null;
 
 function initKeys() {
     for (var i = 0; i < 256; ++i) {
@@ -31,137 +38,125 @@ window.onkeyup = function(event) {
 window.onkeydown = function(event) {
     keys[event.keyCode] = 1;
 };
+
+/*
 window.ontouchstart = function(event) {
-	startDrag(event.touches[0].clientX,event.touches[0].clientY);
+    startDrag(event.touches[0].clientX,event.touches[0].clientY);
 };
 window.onmousedown = function(event) {
-	startDrag(event.clientX,event.clientY);
+    startDrag(event.clientX,event.clientY);
 };
 window.ontouchmove = function(event) {
-	dragCanvas(event.touches[0].clientX,event.touches[0].clientY);
+    dragCanvas(event.touches[0].clientX,event.touches[0].clientY);
 };
 window.onmousemove = function(event) {
-	dragCanvas(event.clientX,event.clientY);
+    dragCanvas(event.clientX,event.clientY);
 };
 window.ontouchend = function(event) {
-	endDrag();
+    endDrag();
 };
 window.onmouseup = function(event) {
-	endDrag();
+    endDrag();
 };
 
 function startDrag(x,y) {
-	draggingCanvas = true;
-	pressForward = false;
-	activeDrag = false;
-	dragVector = {x: x, y: y};
-    	setTimeout(function(){if(draggingCanvas && !activeDrag){pressForward = true;}}, 500);
+    draggingCanvas = true;
+    pressForward = false;
+    activeDrag = false;
+    dragVector = {x: x, y: y};
+    setTimeout(function(){if(draggingCanvas && !activeDrag){pressForward = true;}}, 500);
 };
 
 function endDrag() {
-	draggingCanvas = false;
-	pressForward = false;
-	activeDrag = false;
-	dragVector = {x: null, y: null};
+    draggingCanvas = false;
+    pressForward = false;
+    activeDrag = false;
+    dragVector = {x: null, y: null};
 };
 
 function dragCanvas(x,y) {
-	if(dragVector.x == null || dragVector.y == null) {
-		dragVector.x = x;
-		dragVector.y = y;
-	}
-	if(draggingCanvas && Math.abs(x-dragVector.x) > 2 && Math.abs(y-dragVector.y) > 2) {
-		pressForward = false;
-		activeDrag = true;
-		var right = Vector.crossProduct(cameraDirection, upDirection);
-		if(y < dragVector.y) {
-			cameraDirection = cameraDirection.rotate(right, turnRate*((dragVector.y-y)/10));
-			upDirection = upDirection.rotate(right, turnRate*((dragVector.y-y)/10));
+    if(dragVector.x == null || dragVector.y == null) {
+	dragVector.x = x;
+	dragVector.y = y;
+    }
+    if(draggingCanvas && Math.abs(x-dragVector.x) > 2 && Math.abs(y-dragVector.y) > 2) {
+	pressForward = false;
+	activeDrag = true;
+	var right = Vector.crossProduct(cameraDirection, upDirection);
+	if(y < dragVector.y) {
+	    cameraDirection = cameraDirection.rotate(right, turnRate*((dragVector.y-y)/10));
+	    upDirection = upDirection.rotate(right, turnRate*((dragVector.y-y)/10));
 
-		} else if(y > dragVector.y) {
-			cameraDirection = cameraDirection.rotate(right, -turnRate*((y-dragVector.y)/10));
-			upDirection = upDirection.rotate(right, -turnRate*((y-dragVector.y)/10));
-		}
-		if(x < dragVector.x) {
-			cameraDirection = cameraDirection.rotate(upDirection, -turnRate*((dragVector.x-x)/10));
-		} else if(x > dragVector.x) {
-			cameraDirection = cameraDirection.rotate(upDirection, turnRate*((x-dragVector.x)/10));
-		}
-		dragVector.x = x;
-		dragVector.y = y;
-	} else if(draggingCanvas) {
-		activeDrag = false;
-    		setTimeout(function(){if(draggingCanvas && !activeDrag){pressForward = true;}}, 1000);
+	} else if(y > dragVector.y) {
+	    cameraDirection = cameraDirection.rotate(right, -turnRate*((y-dragVector.y)/10));
+	    upDirection = upDirection.rotate(right, -turnRate*((y-dragVector.y)/10));
 	}
-	
+	if(x < dragVector.x) {
+	    cameraDirection = cameraDirection.rotate(upDirection, -turnRate*((dragVector.x-x)/10));
+	} else if(x > dragVector.x) {
+	    cameraDirection = cameraDirection.rotate(upDirection, turnRate*((x-dragVector.x)/10));
+	}
+	dragVector.x = x;
+	dragVector.y = y;
+    } else if(draggingCanvas) {
+	activeDrag = false;
+    	setTimeout(function(){if(draggingCanvas && !activeDrag){pressForward = true;}}, 1000);
+    }
+    
 };
+*/
 
 function renderStar(context, screenX, screenY, area, color) {
-    var radius = Math.sqrt(area / Math.PI);
-    var alpha = 1;
-    if (radius < 0.5) {
-	radius = 0.5;
-	alpha = 4 * area / Math.PI;
-    }
-    var gradient = context.createRadialGradient(screenX, screenY, 0,
-						screenX, screenY, radius);
-    var rgb = color['r'] + ',' + color['g'] + ',' + color['b'];
-    gradient.addColorStop(0, 'rgba(255,255,255,' + alpha + ')');
-    gradient.addColorStop(1, 'rgba(' + rgb + ',0)');
-    context.fillStyle = gradient;
-    context.beginPath();
-    context.arc(screenX, screenY, radius, 0, 2 * Math.PI);
-    context.fill();
+    
 }
 
+// Direction vectors in camera local coordinates
+var fwd = new THREE.Vector3(0, 0, -1);
+var up = new THREE.Vector3(0, 1, 0);
+var right = new THREE.Vector3(1, 0, 0);
 function doOneFrame() {
-    //if (keys[87] ) {
+    // Schedule this function
+    requestAnimationFrame( doOneFrame );
+
+    // Handle WASD for translation
     if (keys[87] || pressForward) {
-	cameraPosition.addInPlace(cameraDirection.multiplyScalar(speed));
+	camera.translateOnAxis(fwd, speed);
     }
     if (keys[83]) {
-	cameraPosition.addInPlace(cameraDirection.multiplyScalar(-speed));
+	camera.translateOnAxis(fwd, -speed);
     }
-    var right = Vector.crossProduct(cameraDirection, upDirection);
     if (keys[65]) {
-	cameraPosition.addInPlace(right.multiplyScalar(-speed));
+	camera.translateOnAxis(right, -speed);
     }
     if (keys[68]) {
-	cameraPosition.addInPlace(right.multiplyScalar(speed));
+	camera.translateOnAxis(right, speed);
     }
+    if (keys[69]) {
+        camera.translateOnAxis(up, speed);
+    }
+    if (keys[67]) {
+        camera.translateOnAxis(up, -speed);
+    }
+
+    // Handle arrow keys for rotation
     if (keys[38]) {
-	cameraDirection = cameraDirection.rotate(right, -turnRate);
-	upDirection = upDirection.rotate(right, -turnRate);
+        // Up
+        camera.rotateOnAxis(right, -turnRate);
     }
     if (keys[40]) {
-	cameraDirection = cameraDirection.rotate(right, turnRate);
-	upDirection = upDirection.rotate(right, turnRate);
+        // Down
+	camera.rotateOnAxis(right, turnRate);
     }
     if (keys[37]) {
-	cameraDirection = cameraDirection.rotate(upDirection, turnRate);
+        // Left
+        camera.rotateOnAxis(up, turnRate);
     }
     if (keys[39]) {
-	cameraDirection = cameraDirection.rotate(upDirection, -turnRate);
+        // Right
+        camera.rotateOnAxis(up, -turnRate);
     }
-    // Check if we need to re-render
-    if (cameraDirection.isEqual(lastRenderDirection) &&
-        cameraPosition.isEqual(lastRenderPosition) && !starsUpdated) {
-        setTimeout(doOneFrame, 10);
-        return;
-    }
-    lastRenderPosition.copyFrom(cameraPosition);
-    lastRenderDirection.copyFrom(cameraDirection);
-    document.getElementById('controlsBody').innerHTML = "Position: "
-	+cameraPosition.print()+"<br>Direction: "+cameraDirection.print();    
-    starsUpdated = false;
+    camera.updateProjectionMatrix();
     
-    var canvas = document.getElementById('stars');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    var context = canvas.getContext('2d');
-    context.fillStyle = 'black';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
     var starRenderCount = 0;
     var starSkipCount = 0;
     for (ix in visibleOctants) {
@@ -173,55 +168,44 @@ function doOneFrame() {
         for (var i = 0; i < stars.length; ++i) {
 	    var star = stars[i];
 
-            var position = new Vector(star.x, star.y, star.z);
-	    var translated = position.subtract(cameraPosition);
+            var position = new THREE.Vector3(star.x, star.y, star.z);
+	    var translated = new THREE.Vector3();
+            translated.subVectors(position, camera.position);
+            
 	    var brightness = star.lum /
-                (4 * Math.PI * translated.squaredLength());
-            if (brightness < 0.001) {
+                (4 * Math.PI * translated.lengthSq());
+            if (brightness < minNearFieldLum) {
                 starSkipCount++;
                 continue;
             }
-            var scaling = 255 * 8;
-            brightness *= scaling;
             
-	    var projected = translated.basisProjection(right,
-						       cameraDirection,
-						       upDirection);
-	    if (projected.y < 0.00001) {
-	        continue;
-	    }
-	    var sx = (canvas.width / 2) +
-                (canvas.width / 2) * projected.x / projected.y;
-	    var sy = (canvas.height / 2) +
-                (canvas.width / 2) * projected.z / projected.y;
-            if (sx < 0 || sx > canvas.width) continue;
-            if (sy < 0 || sy > canvas.height) continue;
-
 	    var color = {"r":star.r, "g":star.g, "b":star.b};
-	    renderStar(context, sx, sy, brightness, color);
+            // TODO: Render the star
             starRenderCount++;
         }
     }
     //console.log('Stars Rendered: ' + starRenderCount);
     //console.log('Stars Skipped: ' + starSkipCount);
-    setTimeout(doOneFrame, 10);
+    // Render the scene
+    renderer.render(scene, camera);
 };
 
 // Returns immediately, with callback later.
 function updateStars(force) {
     if (force)
         console.log('Forcing star update')
-    var distFromLastUpdate = Math.sqrt(
-        Vector.subtract(lastUpdatePosition,
-                        cameraPosition).squaredLength())
+    var distVect = new THREE.Vector3();
+    distVect.copy(lastUpdatePosition);
+    distVect.sub(camera.position);
+    var distFromLastUpdate = distVect.length();
     if (distFromLastUpdate < distUpdateThreshold && !force)
         return;
-    lastUpdatePosition.copyFrom(cameraPosition);
-    var x = cameraPosition.x;
-    var y = cameraPosition.y;
-    var z = cameraPosition.z;
+    lastUpdatePosition.copy(camera.position);
+    var x = camera.position.x;
+    var y = camera.position.y;
+    var z = camera.position.z;
     //console.log('x: ' + x + ' y: ' + y + ' z: ' + z);
-    getVisibleOctants(0.001, x, y, z, function(newOcts) {
+    getVisibleOctants(minNearFieldLum, x, y, z, function(newOcts) {
         // Use a temporary so we don't update with this before we
         // actually have all thes tars.
 	var tmpVisibleOctants = newOcts;
@@ -264,10 +248,37 @@ function updateStars(force) {
     });
 }
 
+function initSceneAndCamera() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 90,
+                                          (window.innerWidth /
+                                           window.innerHeight),
+                                          0.1, 1000 );
+    camera.position.copy(initialCameraPosition);
+    camera.quaternion.copy(initialCameraRotation);
+    camera.updateProjectionMatrix();
+}
+
+function initRenderer() {
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+}
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+
 window.onload = function() {
+    initSceneAndCamera();
+    initRenderer();
+    scene.add( cube );
+    
     initKeys();
+
     updateStars(true);
     doOneFrame();
+    
     setInterval(function() {
 	updateStars(false);
     }, 500);
