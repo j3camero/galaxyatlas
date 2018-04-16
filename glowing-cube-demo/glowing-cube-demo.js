72f6f7a91850ba0284f14fee4f63c4315de2ba96
Jeff Cameron <cameron.jp@gmail.com>
2018-04-16 16:26:27 -0700

    Initial setup for demo of glowing cube concept.

---
+++ glowing-cube-demo/glowing-cube-demo.js
@@ -0,0 +1,5 @@
+console.log('Glowing Cube Demo!');
+var canvas = document.getElementById("cubecanvas");
+var context = canvas.getContext("2d");
+context.fillStyle = "black";
+context.fillRect(0, 0, canvas.width, canvas.height);
